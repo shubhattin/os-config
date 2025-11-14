b844e8a461f7038432200dbb854f53b3539345a8
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-11-14 20:16:29 +0530

    more

--- linux/arch/major.sh
+++ linux/arch/major.sh
@@ -1,5 +1,8 @@
 # Includes Important Packages to be installed for current hyprland setup
 
+# Basic Apps
+sudo pacman -S vi vim bat --noconfirm
+
 ## Hyprland tools
 sudo pacman -S hyprshot hypridle hyprlock hyprpaper --noconfirm
 sudo pacman -S swaync waybar dolphin --noconfirm
@@ -13,3 +16,22 @@ sudo pacman -S network-manager-applet --noconfirm
 sudo pacman -S qt6ct breeze breeze-gtk kde-cli-tools qt5ct breeze-icons --noconfirm
 sudo pacman -S adwaita-icon-theme gnome-themes-extra gtk3 gtk4 --noconfirm
 sudo pacman -S xdg-desktop-portal-gtk xdg-desktop-portal-hyprland --noconfirm
+
+# Fonts
+sudo pacman -S \
+  ttf-cascadia-code-nerd \
+  ttf-fira-code \
+  ttf-firacode-nerd \
+  ttf-hack-nerd \
+  ttf-inconsolata-nerd \
+  ttf-inconsolata-lgc-nerd --noconfirm
+sudo pacman -S noto-fonts noto-fonts-extra --noconfirm
+
+# Other KDE Apps
+sudo pacman -S partitionmanager okular haruna ark --noconfirm
+
+# Gnome/Gtk App
+sudo pacman -S gnome-calendar --noconfirm
+
+# Clipboard
+sudo pacman -S copyq wl-clipboard --noconfirm
