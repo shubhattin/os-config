8ad45457be6e764560198756ae5a298040003c8e
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-11-14 14:52:25 +0530

    added hyprland basic apps

---
+++ linux/arch/major.sh
@@ -0,0 +1,15 @@
+# Includes Important Packages to be installed for current hyprland setup
+
+## Hyprland tools
+sudo pacman -S hyprshot hypridle hyprlock hyprpaper --noconfirm
+sudo pacman -S swaync waybar dolphin --noconfirm
+# Fonts
+sudo pacman -S ttf-font-awesome ttf-meslo-nerd --noconfirm
+# Bluetooth and Wifi
+sudo pacman -S bluez bluez-utils blueman --noconfirm
+sudo pacman -S network-manager-applet --noconfirm
+
+# Breeze and GTk Themes
+sudo pacman -S qt6ct breeze breeze-gtk kde-cli-tools qt5ct breeze-icons --noconfirm
+sudo pacman -S adwaita-icon-theme gnome-themes-extra gtk3 gtk4 --noconfirm
+sudo pacman -S xdg-desktop-portal-gtk xdg-desktop-portal-hyprland --noconfirm
